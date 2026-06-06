4493757117f0f21e303563b88d0359f73397b134
Doshik143 <ipz232_ddo@student.ztu.edu.ua>
2026-06-06 16:29:19 +0300

    Add SearchByName & SortByTrophies

--- StatBrawl/MainForm.Designer.cs
+++ StatBrawl/MainForm.Designer.cs
@@ -29,23 +29,16 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StatBrawl));
-            this.txtTag = new System.Windows.Forms.TextBox();
             this.btnLoad = new System.Windows.Forms.Button();
             this.lblTrophies = new System.Windows.Forms.Label();
             this.lblHighest = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
             this.dgvBrawlers = new System.Windows.Forms.DataGridView();
+            this.txtTag = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBrawlers)).BeginInit();
             this.SuspendLayout();
-            // 
-            // txtTag
-            // 
-            this.txtTag.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtTag.Location = new System.Drawing.Point(12, 28);
-            this.txtTag.Name = "txtTag";
-            this.txtTag.Size = new System.Drawing.Size(175, 26);
-            this.txtTag.TabIndex = 0;
-            this.txtTag.Text = "#";
             // 
             // btnLoad
             // 
@@ -91,16 +84,47 @@
             // dgvBrawlers
             // 
             this.dgvBrawlers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvBrawlers.Location = new System.Drawing.Point(12, 165);
+            this.dgvBrawlers.Location = new System.Drawing.Point(12, 224);
             this.dgvBrawlers.Name = "dgvBrawlers";
             this.dgvBrawlers.Size = new System.Drawing.Size(400, 200);
             this.dgvBrawlers.TabIndex = 5;
+            // 
+            // txtTag
+            // 
+            this.txtTag.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtTag.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTag.Location = new System.Drawing.Point(12, 28);
+            this.txtTag.Name = "txtTag";
+            this.txtTag.Size = new System.Drawing.Size(175, 26);
+            this.txtTag.TabIndex = 0;
+            this.txtTag.Text = "#8G08UCPCY";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtSearch.Location = new System.Drawing.Point(237, 192);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(175, 26);
+            this.txtSearch.TabIndex = 6;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblSearch.Location = new System.Drawing.Point(202, 195);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(29, 20);
+            this.lblSearch.TabIndex = 7;
+            this.lblSearch.Text = "🔎:";
             // 
             // StatBrawl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 386);
+            this.ClientSize = new System.Drawing.Size(434, 436);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.dgvBrawlers);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.lblHighest);
@@ -109,10 +133,10 @@
             this.Controls.Add(this.txtTag);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(450, 425);
-            this.MinimumSize = new System.Drawing.Size(450, 425);
+            this.MaximumSize = new System.Drawing.Size(450, 475);
+            this.MinimumSize = new System.Drawing.Size(450, 475);
             this.Name = "StatBrawl";
-            this.Text = "Form1";
+            this.Text = "StatBrawl";
             ((System.ComponentModel.ISupportInitialize)(this.dgvBrawlers)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -120,13 +144,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtTag;
         private System.Windows.Forms.Button btnLoad;
         private System.Windows.Forms.Label lblTrophies;
         private System.Windows.Forms.Label lblHighest;
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.DataGridView dgvBrawlers;
+        private System.Windows.Forms.TextBox txtTag;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblSearch;
     }
 }
 
--- StatBrawl/MainForm.cs
+++ StatBrawl/MainForm.cs
@@ -2,14 +2,39 @@
 using System.Windows.Forms;
 using StatBrawl.Services;
 using StatBrawl.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace StatBrawl
 {
     public partial class StatBrawl : Form
     {
+        private List<Brawler> _brawlers = new List<Brawler>();
+
         public StatBrawl()
         {
             InitializeComponent();
+
+            dgvBrawlers.AutoGenerateColumns = false;
+            dgvBrawlers.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dgvBrawlers.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Brawler",
+                DataPropertyName = "name"
+            });
+
+            dgvBrawlers.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Кубки",
+                DataPropertyName = "trophies"
+            });
+
+            dgvBrawlers.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Рекорд",
+                DataPropertyName = "highestTrophies"
+            });
         }
 
         private async void btnLoad_Click(object sender, EventArgs e)
@@ -24,7 +49,9 @@ namespace StatBrawl
                 lblTrophies.Text = $"Кубки: {player.trophies}";
                 lblHighest.Text = $"Рекорд: {player.highestTrophies}";
 
-                dgvBrawlers.DataSource = player.brawlers;
+                _brawlers = player.brawlers;
+
+                dgvBrawlers.DataSource = player.brawlers.OrderByDescending(b => b.trophies).ToList();
             }
             catch (Exception ex)
             {
@@ -34,6 +61,13 @@ namespace StatBrawl
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
             }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string search = txtSearch.Text.ToLower();
+
+            dgvBrawlers.DataSource = _brawlers.Where(b => b.name.ToLower().Contains(search)).OrderByDescending(b => b.trophies).ToList();
         }
     }
 }
